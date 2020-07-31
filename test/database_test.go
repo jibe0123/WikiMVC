baf39a809b540bb674c21ce91f2dfd1404fdb957
Jibe0123 <Jbagostin@gmail.com>
2020-07-31 11:45:44 +0200

    Add first test to database

---
+++ test/database_test.go
@@ -0,0 +1,39 @@
+package test
+
+import (
+	"github.com/jibe0123/WikiMVC/database"
+	"strings"
+	"testing"
+)
+
+func connectTest(t *testing.T) {
+	tests := []struct {
+		name         string
+		desscription string
+		want         string
+	}{
+		{
+			desscription: "Is there errors",
+			want:         "No errors in err",
+		},
+		{
+			name:         "error",
+			desscription: "Is there errors",
+			want:         "Database configuration error",
+		},
+	}
+
+	for _, oneTest := range tests {
+		t.Run(oneTest.name, func(t *testing.T) {
+			_, err := database.Connect()
+			if err != nil {
+				// Database configuration error
+				isBadDatabaseConfig := strings.Contains(err.Error(), "Access denied for user")
+				if isBadDatabaseConfig {
+					return
+				}
+				t.Errorf("connectTest() = %v, want %v", err.Error(), oneTest.want)
+			}
+		})
+	}
+}
